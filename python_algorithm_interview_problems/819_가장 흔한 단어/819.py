a4c203ddf90029e94500d583819944234eefc054
yerim <sontt4266@naver.com>
2021-12-08 02:32:02 +0900

    1207

---
+++ python_algorithm_interview_problems/819_가장 흔한 단어/819.py
@@ -0,0 +1,21 @@
+import collections
+import re
+
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        # 카운트에 같은 단어 횟수 세서 가장 많은 카운트인 단어 출력
+        # if 금지된 단어이면 카운트 안함. 금지된 단어 리스트에서 pop하면?
+        # 구두점 무시. 단어와 붙어있으면 단어만 셈 <- ??
+        
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+                .lower().split()
+                    if word not in banned]
+        
+        counts = collections.Counter(words)
+        
+        return counts.most_common(1)[0][0]
