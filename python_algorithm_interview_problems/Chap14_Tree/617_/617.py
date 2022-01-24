f14728426dbaa2db51b055f4998c2b2ee6326d7f
yerim <sontt4266@naver.com>
2022-01-24 11:23:19 +0900

    0124

--- python_algorithm_interview_problems/Chap14_Tree/617_/617.py
+++ python_algorithm_interview_problems/Chap14_Tree/617_/617.py
@@ -12,5 +12,34 @@ class Solution(object):
         :rtype: TreeNode
         """
         # 두 트리를 하나로 합치기
-        # 두 노드가 겹치는 경우 노드 값을 병합된 노드의 새 값으로 합침 
-        # 그렇지 않으면 NOT null 노드가 새 트리의 노드로 사용됨
+        # 두 노드 자리가 겹치는 경우 해당 자리의 노드는 두 값을 더함
+        # ex) root1 : 1, root2 : 2 => merge root node : 1+2 = 3
+
+        # 리스트 값 순서대로 sum 하면 되려나? 연결리스트니까 val를 더해야할거고..
+        # -----
+        
+        # max_len = max(len(root1), len(root2)) # 연결리스트라서 길이 셀 수 없음 -> while문으로 바꾸자
+        # result = ''
+        
+        if root1 is None and root2 is None:
+            return None
+        
+        elif root1 is None:
+            result = root2
+        elif root2 is None:
+            result = root1
+        
+        else:    
+            result = TreeNode(root1.val + root2.val)
+            result.left = self.mergeTrees(root1.left, root2.left)
+            result.right = self.mergeTrees(root1.right, root2.right)
+            
+        # while root1 or root2:
+                
+            # sum_val = root1.val + root2.val
+            # result.append(sum_val) # 아 이걸 트리(연결리스트)로 만들어줘야 할 듯
+            # root1.pop()
+            # root2.pop()
+
+            
+        return result
